623acb4cfc6b286c10b37fcd18bef2651c9ca5a6
Mateosacco99 <mateosacco99@gmail.com>
2026-06-22 19:54:17 -0300

    Fix: Espaciado

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -23,7 +23,7 @@ class UsuarioController {
     }
   };
 
- createUsuario = async (req, res) => {
+  createUsuario = async (req, res) => {
     try {
       const { nombre, email, contraseña, rol } = req.body;
       if (!nombre) throw new Error("El nombre es requerido");
@@ -34,7 +34,7 @@ class UsuarioController {
     } catch (error) {
       res.status(400).send({ success: false, message: error.message });
     }
-};
+  };
 
   updateUsuario = async (req, res) => {
     try {
@@ -56,34 +56,34 @@ class UsuarioController {
     }
   };
 
-login = async (req, res) => {
+  login = async (req, res) => {
     try {
-        const { email, contraseña } = req.body;
-        if (!email) throw new Error("El email es requerido");
-        if (!contraseña) throw new Error("La contraseña es requerida");
+      const { email, contraseña } = req.body;
+      if (!email) throw new Error("El email es requerido");
+      if (!contraseña) throw new Error("La contraseña es requerida");
 
-        const token = await this.usuarioService.login({ email, contraseña });
-        console.log("Token generado:", token);
+      const token = await this.usuarioService.login({ email, contraseña });
+      console.log("Token generado:", token);
 
-        res.cookie("token", token, {
-            httpOnly: true,
-            maxAge: 3600000, // 1 hora
-        });
+      res.cookie("token", token, {
+        httpOnly: true,
+        maxAge: 3600000,
+      });
 
-        res.status(200).send({ success: true, message: "Login exitoso" });
+      res.status(200).send({ success: true, message: "Login exitoso" });
     } catch (error) {
-        res.status(400).send({ success: false, message: error.message });
+      res.status(400).send({ success: false, message: error.message });
     }
-};
+  };
 
-me = async (req, res) => {
+  me = async (req, res) => {
     try {
-        const usuario = await this.usuarioService.me(req.usuario);
-        res.status(200).send({ success: true, message: usuario });
+      const usuario = await this.usuarioService.me(req.usuario);
+      res.status(200).send({ success: true, message: usuario });
     } catch (error) {
-        res.status(400).send({ success: false, message: error.message });
+      res.status(400).send({ success: false, message: error.message });
     }
-};
+  };
 
   logout = async (req, res) => {
     try {
--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
